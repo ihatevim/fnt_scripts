5bdab658916397ab0435f5c535411d060e33a318
ihatevim <ihatevim@gmail.com>
2021-05-06 23:25:04 -0400

    sa
    as

--- pt_switcher.ts
+++ pt_switcher.ts
@@ -8,6 +8,15 @@ const RootMenu = Menu.AddEntryDeep(["Utility", "Mana Abuse"])
 const State = RootMenu.AddToggle("State")
 
 EventsSDK.on("PrepareUnitOrders", order => {
+	ExecuteOrder.PrepareOrder({
+		orderType: order.OrderType,
+		target: order.Target,
+		position: order.Position,
+		ability: order.Ability,
+		issuers: order.Issuers,
+		queue: true,
+		showEffects: true
+		})
 	const ent = order.Issuers[0],
 		abil = order.Ability
 	if (
@@ -60,15 +69,7 @@ EventsSDK.on("PrepareUnitOrders", order => {
 		
 	if (use_stick)
 		ent.CastNoTarget(stick!, order.Queue)
-		ExecuteOrder.PrepareOrder({
-			orderType: order.OrderType,
-			target: order.Target,
-			position: order.Position,
-			ability: order.Ability,
-			issuers: order.Issuers,
-			queue: true,
-			showEffects: true
-			})
+
 		order.ExecuteQueued
 	return false
 
